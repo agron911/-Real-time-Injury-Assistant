21f0333cac64a19f6f705132c051ff030af3db1f
Kaushikdeka12 <kaushikd@andrew.cmu.edu>
2024-02-02 12:44:36 -0800

    Set the folder stucture

--- Backend/route/index.js
+++ Backend/route/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const passport = require('passport');
+// const passport = require('passport');
 
 // assuming html files stored in view
 const viewDirectory = path.join(__dirname, '..', 'view');
 
 router.get("/main", function (req, res) {
     // assuming there's a main html
-    res.sendFile(path.join(viewDirectory,'main.html'));
+    // res.sendFile(path.join(viewDirectory,'main.html'));
 });
 
 router.post("/register", function (req, res) {
@@ -18,10 +18,10 @@ router.post("/register", function (req, res) {
 });
 
 // login authentication using local strategy
-router.post("/login", passport.authenticate('local', {
-    successRedirect: "/chatroom",
-    failureRedirect: "/main?error=WrongPassword"
-}));
+// router.post("/login", passport.authenticate('local', {
+//     successRedirect: "/chatroom",
+//     failureRedirect: "/main?error=WrongPassword"
+// }));
 
 
 module.exports = router;
