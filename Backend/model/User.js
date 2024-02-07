4df7fb028b00042fdaf94c01976e513953a75c97
Kaushikdeka12 <kaushikd@andrew.cmu.edu>
2024-02-06 19:46:21 -0800

    Add modals, change buttons, acknowlege modal

--- Backend/model/User.js
+++ Backend/model/User.js
@@ -8,6 +8,10 @@ const UserSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true
+    },
+    acknowledged:{
+        type: Boolean,
+        required: false,
     }
 })
 
