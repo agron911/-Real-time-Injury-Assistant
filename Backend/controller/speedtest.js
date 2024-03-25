624e8f3840b0114ee53331fe51a192eb4a181bb3
daniilturpitkacmu <dturpitk@andrew.cmu.edu>
2024-03-25 11:18:31 -0700

    fix

--- Backend/controller/speedtest.js
+++ Backend/controller/speedtest.js
@@ -1,5 +1,5 @@
 import { io } from "../utils/socketSetup.js";
-import { connect,} from './db-handler';
+import { connect,} from '../tests/db-handler.js';
 
 export async function suspendNormalOps(req, res){
     io.emit("suspendNormalOps");
--- Backend/route/router.js
+++ Backend/route/router.js
@@ -31,7 +31,7 @@ router.get("/user/status/:username", getStatus);
 
 router.patch("/auth/users", loginOrLogout);
 router.post("/sockets/users/:username", registerUserSocket);
-router.get("/messages/public/search/:content", searchByPublicMessage);
+router.get("/messages/public", searchByPublicMessage);
 router.get("/messages/private/search/:sender/:receiver/:content", searchByPrivateMessages);
 router.get("/users/username/search/:user", searchByUsername);
 router.get("/users/messages/announcement/search/:username", searchByAnnouncement);
