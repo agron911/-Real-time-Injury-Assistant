88e6ce035febe68d457526b4f2276525aab49c63
daniilturpitkacmu <dturpitk@andrew.cmu.edu>
2024-04-10 17:51:15 -0700

    test fixes11

--- Backend/tests/test-api.js
+++ Backend/tests/test-api.js
@@ -467,9 +467,7 @@ describe("Facilities operations tests", ()=>{
         }
         await request(Server.instance.httpServer).post("/facilities/newfacility").send(data2)
         let results = await request(Server.instance.httpServer).get("/facility/search?description=Open-Wound&mobility=No").send()
-        results.json().forEach(facility=>{
-            expect(facility.body.searchresult.type).toBe("Emergency Room")
-        })
+        expect(results.body.searchresult.type).toBe("Emergency Room")
     })
     
 
